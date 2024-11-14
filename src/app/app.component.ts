f968c8c88100394ea158c1d4e3f9fe4acda1709d
Varghese Abraham <varnit2010@gmail.com>
2024-11-13 19:50:35 -0500

    Add shared module, ionic storage pkg, storage service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { CoreModule } from './core/core.module';
+import { Storage } from '@ionic/storage-angular';
+import { LoggerService } from './core/services/logger/logger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, CoreModule],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  logger = inject(LoggerService);
+  constructor(private storage: Storage) { }
+
+  ngOnInit(): void {
+    this.storage
+      .create()
+      .then(() => this.logger.info({
+        tag: 'AppComponent',
+        message: 'Storage initialized'
+      }));
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+import { Drivers } from '@ionic/storage';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    importProvidersFrom(IonicStorageModule.forRoot({
+      name: '__dyshDb',
+      driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage],
+    }))
   ],
 });
